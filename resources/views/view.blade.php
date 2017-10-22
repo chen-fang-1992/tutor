7fd56742e96f84e9b252a27456c4e734b18c8817
chen-fang-1992 <fc812168079@gmail.com>
2017-10-23 00:24:04 +1100

    fix CSRF token not found

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta name="csrf-token" content="{{ csrf_token() }}">
 			<title>TUTOR</title>
 			<!-- Fonts -->
 			<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,700,700i" rel="stylesheet" type="text/css">
